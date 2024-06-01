3d3db276b45bf0df9466cd7cf1fd0e18559e9ae6
Nahuel Jaume <nahueljaume@gmail.com>
2024-06-01 12:35:20 -0300

    tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter, NotFoundRoute } from '@tanstack/react-router';
+import { Route as rootRoute } from './routes/__root.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Import the generated route tree
+import { routeTree } from './routeTree.gen';
+
+// Create a new router instance
+const notFoundRoute = new NotFoundRoute({
+    getParentRoute: () => rootRoute,
+    component: () => '404 Not Found',
+});
+
+// Create a new router instance
+const router = createRouter({ routeTree, notFoundRoute });
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router;
+    }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!;
+if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <StrictMode>
+            <RouterProvider router={router} />
+        </StrictMode>
+    );
+}
